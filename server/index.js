488905bf315e8e9bc2ca91f3466e40bd0595bcd0
Marek Valko <valko.marek@gmail.com>
2019-10-06 09:54:51 +0200

    prepared for deployment

--- server/index.js
+++ server/index.js
@@ -6,15 +6,24 @@ const app = express();
 
 //Middle ware
 
-const posts = require("./routes/api/posts")
-
+const posts = require("./routes/api/posts");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/api/posts', posts)
+app.use("/api/posts", posts);
 
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handle SPA
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
 
 const port = process.env.PORT | 5000;
 
